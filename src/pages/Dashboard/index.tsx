e1b7696cf5a33b7aede20c5fc2e42f180109ed8d
Igor Godoy <godoy.ivgm@gmail.com>
2020-06-03 21:17:22 -0300

    first commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import Header from '../../components/Header';
+
+import api from '../../services/api';
+
+import Food from '../../components/Food';
+import ModalAddFood from '../../components/ModalAddFood';
+import ModalEditFood from '../../components/ModalEditFood';
+
+import { FoodsContainer } from './styles';
+
+interface IFoodPlate {
+  id: number;
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+  available: boolean;
+}
+
+const Dashboard: React.FC = () => {
+  const [foods, setFoods] = useState<IFoodPlate[]>([]);
+  const [editingFood, setEditingFood] = useState<IFoodPlate>({} as IFoodPlate);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+
+  useEffect(() => {
+    async function loadFoods(): Promise<void> {
+      const response = await api.get('/foods');
+
+      setFoods(response.data);
+    }
+
+    loadFoods();
+  }, []);
+
+  const handleAddFood = useCallback(
+    async (food: Omit<IFoodPlate, 'id' | 'available'>): Promise<void> => {
+      try {
+        const response = await api.post('/foods', { available: true, ...food });
+
+        const { data: newFood } = response;
+
+        setFoods([...foods, newFood]);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    [foods],
+  );
+
+  const handleUpdateFood = useCallback(
+    async (food: Omit<IFoodPlate, 'id' | 'available'>): Promise<void> => {
+      await api.put(`/foods/${editingFood.id}`, food);
+
+      const newFoodsArray = foods;
+
+      const editedFoodIndex = newFoodsArray.findIndex(
+        findFood => findFood.id === editingFood.id,
+      );
+
+      newFoodsArray[editedFoodIndex] = {
+        ...food,
+        id: newFoodsArray[editedFoodIndex].id,
+        available: newFoodsArray[editedFoodIndex].available,
+      };
+
+      setFoods([...newFoodsArray]);
+    },
+    [editingFood, foods],
+  );
+
+  const handleDeleteFood = useCallback(
+    async (id: number): Promise<void> => {
+      await api.delete(`/foods/${id}`);
+      setFoods([...foods.filter(food => food.id !== id)]);
+    },
+    [foods],
+  );
+
+  const toggleModal = useCallback((): void => {
+    setModalOpen(!modalOpen);
+  }, [modalOpen]);
+
+  const toggleEditModal = useCallback((): void => {
+    setEditModalOpen(!editModalOpen);
+  }, [editModalOpen]);
+
+  const handleEditFood = useCallback(
+    (food: IFoodPlate): void => {
+      setEditingFood(food);
+      setEditModalOpen(!editModalOpen);
+    },
+    [editModalOpen],
+  );
+
+  return (
+    <>
+      <Header openModal={toggleModal} />
+      <ModalAddFood
+        isOpen={modalOpen}
+        setIsOpen={toggleModal}
+        handleAddFood={handleAddFood}
+      />
+      <ModalEditFood
+        isOpen={editModalOpen}
+        setIsOpen={toggleEditModal}
+        editingFood={editingFood}
+        handleUpdateFood={handleUpdateFood}
+      />
+
+      <FoodsContainer data-testid="foods-list">
+        {foods &&
+          foods.map(food => (
+            <Food
+              key={food.id}
+              food={food}
+              handleDelete={handleDeleteFood}
+              handleEditFood={handleEditFood}
+            />
+          ))}
+      </FoodsContainer>
+    </>
+  );
+};
+
+export default Dashboard;
